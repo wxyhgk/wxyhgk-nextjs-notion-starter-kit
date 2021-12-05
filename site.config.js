2206ba8305c38ca7f01746262dd21cc472b72f78
wxyhgk <3123624718@qq.com>
2021-12-05 13:12:54 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ site.config.js
@@ -0,0 +1,51 @@
+module.exports = {
+  // where it all starts -- the site's root Notion page (required)
+  rootNotionPageId: 'f36f4895ff9c41dea64b9ab17fd596e5',
+
+  // if you want to restrict pages to a single notion workspace (optional)
+  // (this should be a Notion ID; see the docs for how to extract this)
+  rootNotionSpaceId: null,
+
+  // basic site info (required)
+  name: '我心永恒的小站',
+  domain: 'https://scandalous-draw-945.notion.site/Blog-Notion-89abd385725e427ba86f8dd6380d5ffc',
+  author: '我心永恒',
+
+  // open graph metadata (optional)
+  description: 'Example site description',
+  socialImageTitle: 'Transitive Bullshit',
+  socialImageSubtitle: 'Hello World! 👋',
+
+  // social usernames (optional)
+  // twitter: 'transitive_bs',
+  github: 'wxyhgk',
+  // linkedin: 'fisch2',
+
+  // default notion icon and cover images for site-wide consistency (optional)
+  // page-specific values will override these site-wide defaults
+  defaultPageIcon: null,
+  defaultPageCover: null,
+  defaultPageCoverPosition: 0.5,
+
+  // image CDN host to proxy all image requests through (optional)
+  // NOTE: this requires you to set up an external image proxy
+  imageCDNHost: null,
+
+  // Utteranc.es comments via GitHub issue comments (optional)
+  utterancesGitHubRepo: null,
+
+  // whether or not to enable support for LQIP preview images (optional)
+  // NOTE: this requires you to set up Google Firebase and add the environment
+  // variables specified in .env.example
+  isPreviewImageSupportEnabled: false,
+
+  // map of notion page IDs to URL paths (optional)
+  // any pages defined here will override their default URL paths
+  // example:
+  //
+  // pageUrlOverrides: {
+  //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
+  //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
+  // }
+  pageUrlOverrides: null
+}
